8a306548cce271725978f3769ae4b55919c2954b
Abhinav <abhinav.varma123@gmail.com>
2016-09-24 15:28:46 -0700

    User and Child Apps created

---
+++ ChildApp/models.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# class Child(models.Model):
+#     first_name = models.CharField(max_length=30)
+#     last_name = models.CharField(max_length=30)
---
+++ UserApp/models.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+import datetime
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_joined = models.DateField(default=datetime.datetime.utcnow().date())
+    date_relieved = models.DateField(null=True)
