281ba7a31ba4ba72a20a3f5fea5f078287e1bc89
Abhinav <abhinav.varma123@gmail.com>
2016-09-24 20:18:36 -0700

    static files

--- ChildApp/views.py
+++ ChildApp/views.py
@@ -5,11 +5,9 @@ from django.shortcuts import render
 from ChildApp.models import Child
 
 def dashboard(request):
-    return render(request, "index.html")
 
-# def dashboard(request):
-#     results = search(cm_list=[])
-#     return JsonResponse()
+    results = search(cm_list=[request.user.pk])
+    return JsonResponse()
 
 def search(first_name_list=None, middle_name_list=None, last_name_list=None, cm_list=None):
     query = {}
@@ -20,7 +18,7 @@ def search(first_name_list=None, middle_name_list=None, last_name_list=None, cm_
     if last_name_list and isinstance(last_name_list, list):
         query["last_name__in"] = last_name_list
     if cm_list and isinstance(cm_list, list):
-        query["cm_list"] = cm_list
+        pass
     children = Child.objects.filter(**query)
     
     serialized = []
