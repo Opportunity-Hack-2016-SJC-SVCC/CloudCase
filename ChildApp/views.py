683f92d1bd6f5700191a440316077f6d208f354a
Nishanth Reddy <reddynishanth16@gmail.com>
2016-09-24 17:59:55 -0700

    updating login view

--- ChildApp/views.py
+++ ChildApp/views.py
@@ -1,5 +1,9 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
 def dashboard(request):
-    return render(request, "index.html")
+    return render(request, "MyBriefcaseLoginPage.html")
+
+def jsonResponse(request):
+    return JsonResponse({"name" : "nishanth"})
